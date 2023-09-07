c79bf057656c5c0cf0e9c51647bb6abff066738d
Joi Misenti <jmisenti@192.168.1.12>
2023-09-07 16:24:46 -0300

    working on part 3

---
+++ starter/src/main/java/com/devmountain/noteApp/services/UserServiceImpl.java
@@ -0,0 +1,11 @@
+package com.devmountain.noteApp.services;
+
+import com.devmountain.noteApp.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl {
+    @Autowired
+    private UserRepository userRepository;
+}
