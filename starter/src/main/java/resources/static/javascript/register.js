bce1c5106f5e4b8a5f3b94447658824a87426a42
Joi Misenti <jmisenti@Jois-MacBook-Air.local>
2023-09-11 09:08:42 -0300

    adding JavaScript part 4

---
+++ starter/src/main/java/resources/static/javascript/register.js
@@ -0,0 +1,4 @@
+const registerForm = document.getElementById('register-form')
+const registerUsername = document.getElementById('register-username')
+const registerPassword = document.getElementById('register-password')
+
